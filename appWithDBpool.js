8969cbb6fd15c9b9291afcf2670ea0db36a75983
Abdullah Al Mahmud <abdullahalmahmud325@gmail.com>
2024-02-08 04:52:01 +0600

    modified search,login etc

--- appWithDBpool.js
+++ appWithDBpool.js
@@ -7,10 +7,12 @@ const oracledb = require('./DB/dbConnect.js');
 const app = express();
 app.set("view engine","ejs");
 
+//app.use(express.static("views/public"));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended : true}));
 app.use(morgan('dev'));
+
 
 // creating Server
 const PORT = 3000;
---
+++ views/public/script/script.js
@@ -0,0 +1,66 @@
+const fetchData= () => {
+const name = document.getElementById("searchBar").value
+
+fetch(`/search${name}`)
+.then(res=>res.json())
+.then((data) => {
+    document.getElementById("searchRes").innerHTML+="1234"
+})}
+
+function addToCart(button) {
+var car_id = button.dataset.info;
+fetch('', {
+
+})
+}
+
+
+function fetchResults(button) {
+// Access the text content of the clicked button
+var buttonText = button.textContent || button.innerText;
+//console.log(buttonText);
+// Log or perform other actions with the button text
+fetch('/showBrandWise',{
+method: 'POST',
+headers: {
+      'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({buttonText})
+})
+.then(res=>res.json())
+.then(data=>{
+sessionStorage.setItem('data', JSON.stringify(data));
+document.getElementById('searchRes').innerHTML="";
+for(let i=0;i<data.length;i++) {
+document.getElementById('searchRes').innerHTML+=`
+<div class="card mb-3">
+<div class="row no-gutters">
+  <div class="col-md-4">
+    <a href="https://www.google.com/">
+      <img src="${data[i].CAR_IMAGE_URL}" class="card-img" style="height: 100%;"  alt="..." >
+    </a>
+  </div>
+  <div class="col-md-8">
+    <div class="card-body">
+      <a class="card-title" href="">${data[i].MODEL_NAME}</a>
+      <p class="card-text">Price : ${data[i].PRICE}</p>
+      <button class="btn btn-primary" name="add_to_cart_button" data-info="${data[i].MODEL_COLOR_ID}">Add to cart</button>
+    </div>
+  </div>
+</div>
+</div>`
+}
+var add_to_cart_buttons = document.getElementsByName('add_to_cart_button');
+
+for(var i = 0;i<add_to_cart_buttons.length;i++) {
+add_to_cart_buttons[i].addEventListener('click',function(){
+  addToCart(this);
+});
+}
+})
+}
+
+function toggleSidebar() {
+var sidebar = document.getElementById('sidebar');
+sidebar.style.right= sidebar.style.right === '0px' ? '-250px' : '0px';
+}
