e6f0d3e7f74b7b2892c7be3ff3dcb8eafbe47760
Abdullah Al Mahmud <abdullahalmahmud325@gmail.com>
2024-02-24 02:39:51 +0600

    customer information update ready

--- controller/logIn.js
+++ controller/logIn.js
@@ -79,9 +79,29 @@ async  function sendUserData ( email, password )
     }
 }
 
+async  function sendCustomerData ( email, password )
+{
+    try{
+        const sql =`
+            SELECT U.ID,U.NAME,U.EMAIL,U.PHONE_NUMBER,NVL(C.LOCATION_ID,0) AS LOCATION_ID
+            FROM USERS U
+            JOIN CUSTOMER C ON (U.ID = C.ID)
+            WHERE U.EMAIL = :email AND (U.PASSWORD = :password)
+        `
+        const binds = { email, password };
+
+        const result = await execute( sql, binds );
+        return result;
+
+    }catch(err){
+        console.log(err);
+    }
+}
+
 module.exports = {
     authUser,
     sendUserData,
     sendUserDataByUserName,
-    sendUserDataByID
+    sendUserDataByID,
+    sendCustomerData
 }
